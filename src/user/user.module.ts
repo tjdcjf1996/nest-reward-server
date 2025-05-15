ce298a8bade9c31fcb6a35b56ef5421abd6a0ad1
GDORI <r_louis@naver.com>
2025-05-15 20:13:19 +0900

    feat: 유저 모듈 생성

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { UserController } from './user.controller';
+import { User, UserSchema } from './schemas/user.schema.js';
+import { UserService } from './user.service.js';
 import { MongooseModule } from '@nestjs/mongoose';
 import { jwtConfig } from 'src/config/db/jwt.config';
+
+@Module({
+  imports: [
     JwtModule.registerAsync(jwtConfig),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
+})
+export class UserModule {}
