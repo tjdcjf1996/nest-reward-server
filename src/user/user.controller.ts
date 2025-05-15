4ea8125a7edd6bf784878cc807391ce83a7e6311
GDORI <r_louis@naver.com>
2025-05-15 20:13:30 +0900

    feat: 유저 컨트롤러 생성

---
+++ src/user/user.controller.ts
@@ -0,0 +1,31 @@
+import { UserInfo } from 'src/utils/userInfo.decorator';
+
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { LoginDto } from './dto/login.dto';
+import { UserService } from './user.service';
+import { User } from './schemas/user.schema.js';
+import { Role } from './types/userRole.type.js';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() loginDto: LoginDto) {
+    return await this.userService.register(loginDto.email, loginDto.password);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return await this.userService.login(loginDto.email, loginDto.password);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('role')
+  getEmail(@UserInfo() user: User) {
+    return { email: user.email, role: Role[user.role] };
+  }
+
+  
+}
