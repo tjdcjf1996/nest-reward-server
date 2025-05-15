1435aac1d4d9bbe15530a6f3b8a800cedf7a8d69
GDORI <r_louis@naver.com>
2025-05-16 06:09:42 +0900

    feat: mongodb config 추가

---
+++ src/config/env/validation.config.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  // MongoDB
+  MONGO_USER: Joi.string().required(),
+  MONGO_PASS: Joi.string().required(),
+  MONGO_HOST: Joi.string().required(),
+  MONGO_PORT: Joi.number().required(),
+  MONGO_DB: Joi.string().required(),
+});
