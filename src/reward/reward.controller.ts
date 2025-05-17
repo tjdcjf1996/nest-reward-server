9bcd6236cca77d6b008c7ca5b741c63115c3a4f1
GDORI <r_louis@naver.com>
2025-05-17 13:24:28 +0900

    feat: 리워드 컨트롤러 추가

--- src/reward/reward.controller.ts
+++ src/reward/reward.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { RewardService } from './reward.service.js';
+import { CreateRewardDto } from './dto/create-reward.dto.js';
+import { UpdateRewardDto } from './dto/update-reward.dto.js';
 
 @Controller('reward')
-export class RewardController {}
+export class RewardController {
+  constructor(private readonly rewardService: RewardService) {}
+
+  @Post()
+  async create(@Body() createRewardDto: CreateRewardDto) {
+    return this.rewardService.create(createRewardDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.rewardService.findAll();
+  }
+
+  @Get('/all')
+  async findAllIncDeleted() {
+    return this.rewardService.findAllIncDeleted();
+  }
+
+  @Get(':eventId')
+  async findByEvent(@Param('eventId') eventId: string) {
+    return this.rewardService.findByEventId(eventId);
+  }
+
+  @Put(':eventId')
+  async update(
+    @Param('eventId') eventId: string,
+    @Body() updateRewardDto: UpdateRewardDto,
+  ) {
+    return this.rewardService.update(eventId, updateRewardDto);
+  }
+
+  @Delete(':eventId')
+  async delete(@Param('eventId') eventId: string) {
+    return this.rewardService.delete(eventId);
+  }
+}
