18ccd5f6a92cb43da8ef9fb7c1aa42e5d9ea2206
GDORI <r_louis@naver.com>
2025-05-17 16:47:25 +0900

    feat: event-execute 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { validationSchema } from './config/env/validation.config';
 import databaseConfig from './config/env/database.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongoURI } from './config/db/database.config';
+import { EventExecuteModule } from './event-execute/event-execute.module';
 import { RewardModule } from './reward/reward.module';
 
 const configModuleSetting = {
@@ -23,6 +24,7 @@ const configModuleSetting = {
     MongooseModule.forRootAsync(mongoURI),
     // Event 모듈 추가
     EventModule,
+    EventExecuteModule,
     RewardModule,
   ],
   controllers: [AppController],
