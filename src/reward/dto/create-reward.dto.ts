cca41daf893dc8138fcb3ea4704db361806ef5df
GDORI <r_louis@naver.com>
2025-05-18 02:20:24 +0900

    refactor: 리워드 등록 dto 변경

--- src/reward/dto/create-reward.dto.ts
+++ src/reward/dto/create-reward.dto.ts
@@ -1,16 +1,31 @@
-import { IsBoolean, IsEnum, IsMongoId, IsNumber } from 'class-validator';
+import {
+  IsBoolean,
+  IsEnum,
+  IsMongoId,
+  IsNumber,
+  IsOptional,
+  Validate,
+} from 'class-validator';
 import { RewardType } from '../../types/reward.type';
+import { CouponRewardDto } from './coupon-reward.dto';
+import { IsRewardDataConstraint } from './validator/reward-data.validator';
 
 export class CreateRewardDto {
   @IsMongoId()
   eventId: string;
 
-  @IsEnum(RewardType)
-  category: RewardType;
+  @IsEnum(RewardType, {
+    message: '유효한 리워드 타입을 입력해주세요. (item, coupon, point, cash)',
+  })
+  rewardType: RewardType;
 
   @IsNumber()
   amount: number;
 
   @IsBoolean()
   autoExecute: boolean;
+
+  @IsOptional()
+  @Validate(IsRewardDataConstraint)
+  rewardData?: CouponRewardDto;
 }
