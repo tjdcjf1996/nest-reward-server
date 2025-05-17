ee6ae93d57e8fff0ac2ef92d93dacb3da10869c2
GDORI <r_louis@naver.com>
2025-05-17 11:50:59 +0900

    update : reward 모듈에 mongoose 모듈 추가

--- src/reward/reward.module.ts
+++ src/reward/reward.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RewardController } from './reward.controller';
 import { RewardService } from './reward.service';
+import { Reward, RewardSchema } from './schemas/reward.schema.js';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [],
+    MongooseModule.forFeature([{ name: Reward.name, schema: RewardSchema }]),
   controllers: [RewardController],
   providers: [RewardService],
 })
