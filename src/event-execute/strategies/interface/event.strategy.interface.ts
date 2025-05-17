b309a8391bc41013366a9225062e326dc9f38ad7
GDORI <r_louis@naver.com>
2025-05-17 23:51:26 +0900

    feat: 이벤트 완료 조건 확인 함수 validate 생성

--- src/event-execute/strategies/interface/event.strategy.interface.ts
+++ src/event-execute/strategies/interface/event.strategy.interface.ts
@@ -1,5 +1,9 @@
 import { EventExecuteDto } from '../../dto/eventExecute.dto';
 
 export interface EventTypeStrategy {
-  handle(eventExecuteDto: EventExecuteDto): Promise<boolean>;
+  handle(eventExecuteDto: EventExecuteDto): Promise<any>;
+  validate(
+    eventExecuteDto: EventExecuteDto,
+    contents: Record<string, any>,
+  ): Promise<boolean>;
 }
