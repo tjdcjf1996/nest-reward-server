57a1211f79243ac0115a4ae311d60417e6ca3dfe
GDORI <r_louis@naver.com>
2025-05-19 04:53:57 +0900

    refactor: 중복 로직 데코레이터로 처리

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -11,38 +11,30 @@ import {
 import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
-import { Request } from 'express';
 import { EventExecuteDto } from '../event/dto/eventExecute.dto';
+import { AuthToken } from '../utils/decorator/auth-token.decorator';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Post()
-  create(@Body() createEventDto: CreateEventDto, @Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
+  create(@Body() createEventDto: CreateEventDto, @AuthToken() token: string) {
     return this.eventService.create(createEventDto, token);
   }
 
   @Get()
-  findAll(@Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
+  findAll(@AuthToken() token: string) {
     return this.eventService.findAll(token);
   }
 
   @Get('/all')
-  findAllIncDeleted(@Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
+  findAllIncDeleted(@AuthToken() token: string) {
     return this.eventService.findAllIncDeleted(token);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string, @Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
+  findOne(@Param('id') id: string, @AuthToken() token: string) {
     return this.eventService.findOne(id, token);
   }
 
@@ -50,24 +42,21 @@ export class EventController {
   update(
     @Param('id') id: string,
     @Body() updateEventDto: UpdateEventDto,
-    @Req() req: Request,
+    @AuthToken() token: string,
   ) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
     return this.eventService.update(id, updateEventDto, token);
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string, @Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
+  async delete(@Param('id') id: string, @AuthToken() token: string) {
     return await this.eventService.delete(id, token);
   }
 
   @Post('/execute')
-  async execute(@Body() eventExecuteDto: EventExecuteDto, @Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
+  async execute(
+    @Body() eventExecuteDto: EventExecuteDto,
+    @AuthToken() token: string,
+  ) {
     return await this.eventService.execute(eventExecuteDto, token);
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,7 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { Public } from '../auth/public/public.decorator';
 import { LoginUserDto } from './dto/login-user.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { Request } from 'express';
+import { AuthToken } from '../utils/decorator/auth-token.decorator';
 
 @Controller('user')
 export class UserController {
@@ -23,18 +23,12 @@ export class UserController {
   }
 
   @Patch('role')
-  updateRole(@Body() updateRoleDto: UpdateRoleDto, @Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
-
+  updateRole(@Body() updateRoleDto: UpdateRoleDto, @AuthToken() token: string) {
     return this.userService.updateRole(updateRoleDto, token);
   }
 
   @Patch('/role/admin')
-  updateAdminRole(@Req() req: Request) {
-    const token = req.headers.authorization;
-    // jwt 토큰이 없으면 실행되지 않기 때문에 undefined일 수 없음
-
+  updateAdminRole(@AuthToken() token: string) {
     return this.userService.updateAdminRole(token);
   }
 }
