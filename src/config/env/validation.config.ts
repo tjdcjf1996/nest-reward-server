abbf3de3ab2c0ded7a6b374f4f60076e498cff1b
GDORI <r_louis@naver.com>
2025-05-19 04:16:42 +0900

    update: env 검증 내용 추가 및 jwt 만료기간 변경

--- src/config/env/validation.config.ts
+++ src/config/env/validation.config.ts
@@ -3,4 +3,18 @@ const Joi = require('joi');
 export const validationSchema = Joi.object({
   // JWT
   JWT_SECRET_KEY: Joi.string().required(),
+
+  // msa 통신용
+  MSA_SERVER_KEY: Joi.string().required(),
+
+  // msa 서버 주소
+  AUTH_SERVER: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .required(),
+  EVENT_SERVER: Joi.string()
+    .uri({ scheme: ['http', 'https'] })
+    .required(),
+
+  // PORT
+  PORT: Joi.number().default(3000),
 });
