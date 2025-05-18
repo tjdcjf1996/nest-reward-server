a98aef7a11a97cfcc8e0b1aedd2792c8e81b3259
GDORI <r_louis@naver.com>
2025-05-19 08:02:32 +0900

    refactor: env에 따른 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
 }
 
 bootstrap();
